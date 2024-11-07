2ba6e8a9073326960d777969762b4fdcc723a600
Russuel <johnlloydrussuel.tengco@gmail.com>
2024-11-07 09:42:20 +0800

    reduce files

--- model/userData.js
+++
@@ -1,8 +0,0 @@
-import connection from './database_config.js'
-
-connection.connect();
-connection.query('SELECT * from users',(err, result) => {
-    if (err) throw err
-    console.log(result)
-});
-connection.end();
